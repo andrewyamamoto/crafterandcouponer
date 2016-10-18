2cf1778f1c2d40178d72de9bead319fcd941af97
andrewyamamoto <andrew.asada@gmail.com>
2016-10-17 22:17:37 -0700

    added themes

---
+++ themes/ever-after/colophon.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Colophon
+ *
+ * This template is displayed just before closing #page.
+ * By default it displays generator link along with the
+ * theme name and the author.
+ *
+ * @package Ever_After
+ * @since Ever After 1.0
+ */
+?>
+
+<footer id="colophon" role="contentinfo">
+	<div id="site-info">
+		
+	</div>
+</footer><!-- #colophon -->
