6296001956bf63a1e8b61665293ea223cb23a6f8
andrewyamamoto <andrew.asada@gmail.com>
2016-10-17 22:10:57 -0700

    test 2

--- themes/crafterandcouponer/index.php
+++ themes/crafterandcouponer/index.php
@@ -16,7 +16,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			blah blah blah
+			blah blah blah tester andrew
 		<?php
 		if ( have_posts() ) :
 
